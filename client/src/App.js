e674dbb8a6da44281e1ade6216e577a2a85b2875
karl <kianrapada@gmail.com>
2025-08-11 10:10:06 -0700

    se

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
-// client/src/App.js
+import React from 'react';
+import './App.css';
+
 function App() {
   return (
-    <div>
-      <h1>Hello, clean React app!</h1>
+    <div className="login-container">
+      <h2>Login</h2>
+      <form>
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Log In</button>
+      </form>
     </div>
   );
 }
 
 export default App;
-  
