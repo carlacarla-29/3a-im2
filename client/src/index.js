4889069847c90833c92ad5c389e63803d13ff732
karl <kianrapada@gmail.com>
2025-08-11 22:51:54 -0700

    done

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
 const express = require("express");
-const app = express();
+const cors = require("cors");
 const { createUserTable } = require("./models/auth-model");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 createUserTable()
   .then(() => console.log("Table user has been created"))
   .catch((error) => console.log(error));
 
-app.use(express.json());
-
 app.use("/api/auth", require("./routes/auth-route"));
 
 app.listen(5000, () => {
-  console.log("Server Running");
+  console.log("Server Running on port 5000");
 });
